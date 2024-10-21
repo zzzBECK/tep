b13267e7a7d4f6b2de573f63751dfa8ce9f8bfe5
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-10-21 10:09:06 -0300

    feat: 1 exec

---
+++ lista_0/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    int x,y;
+
+    cin >> x >> y;
+
+    cout << "SOMA = " << x + y << '\n';
+
+    return 0;
+}
