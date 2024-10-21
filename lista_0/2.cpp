1a6d21abab8c8ad21b7956af9f0092d835476aaa
Alexandre Beck <alexandrebeck2003@gmail.com>
2024-10-21 10:32:14 -0300

    feat: add solution for problem 2

---
+++ lista_0/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+
+    int a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+
+    cout << "DIFERENCA = " << (a * b) - (c * d) << '\n';
+
+    return 0;
+}
